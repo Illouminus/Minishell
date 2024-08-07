65cde27d3dadc54187a4e51b8b65894e6f3b012d
Illouminus <professionnel.baillot@gmail.com>
2024-08-07 11:17:12 +0200

    cleaning and check .H

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: edouard <edouard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 14:39:56 by ebaillot          #+#    #+#             */
-/*   Updated: 2024/08/07 11:02:16 by edouard          ###   ########.fr       */
+/*   Updated: 2024/08/07 11:15:52 by edouard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 /*
 -------------------------------------------------------------
-|                       LIBRAIRIE                            |
+|                       BIBLIOTHÈQUES                       |
 -------------------------------------------------------------
 */
 
@@ -38,11 +38,11 @@ extern int g_exit_code;
 
 /*
 -------------------------------------------------------------
-|                       STRUCTURE                            |
+|                       STRUCTURES                          |
 -------------------------------------------------------------
 */
 
-// Enum Token Type
+// Enumération des types de tokens
 typedef enum e_token_type
 {
 	TOKEN_TYPE_CMD,
@@ -56,111 +56,101 @@ typedef enum e_token_type
 // Structure d'un token
 typedef struct s_token
 {
-	t_token_type tok_type;	  // Type de token (commande, argument, opérateur de redirection)
-	char *tok_value;			  // Valeur du token (texte de la commande ou de l'argument)
-	struct s_token *next_tok; // Pointeur vers le token suivant
-	struct s_token *prev_tok; // Pointeur vers le token précédent
+	t_token_type tok_type;
+	char *tok_value;
+	struct s_token *next_tok;
+	struct s_token *prev_tok;
 } t_token;
 
 // Structure d'une commande
 typedef struct s_command
 {
-	char *cmd_value;				 // Nom de la commande
-	char **cmd_args;				 // Arguments de la commande
-	bool is_builtin_cmd;			 // Indicateur si la commande est une commande intégrée
-	t_token *redir_tokens;		 // Liste des redirections associées à la commande
-	struct s_command *next_cmd; // Pointeur vers la commande suivante
-	struct s_command *prev_cmd; // Pointeur vers la commande précédente
-	char *heredoc_marker;		 // Marqueur pour le heredoc
-	char *input_file;				 // Descripteur de fichier pour la redirection d'entrée
-	char *output_file;			 // Descripteur de fichier pour la redirection de sortie
-	int pipe_fds[2];				 // Descripteurs pour les pipes
+	char *cmd_value;
+	char **cmd_args;
+	bool is_builtin_cmd;
+	t_token *redir_tokens;
+	struct s_command *next_cmd;
+	struct s_command *prev_cmd;
+	char *heredoc_marker;
+	char *input_file;
+	char *output_file;
+	int pipe_fds[2];
 } t_command;
 
-// Structyre pour env
+// Structure pour les variables d'environnement
 typedef struct s_env
 {
-	char *env_var_name;		// Nom de la variable d'environnement
-	char *env_value;			// Valeur de la variable d'environnement
-	struct s_env *next_env; // Pointeur vers la variable d'environnement suivante
+	char *env_var_name;
+	char *env_value;
+	struct s_env *next_env;
 } t_env;
 
+// Structure principale du shell
 typedef struct s_shell
 {
-	char *user_input;			 // Entrée de l'utilisateur
-	t_token *token_list;		 // Liste des tokens générés par le lexer
-	t_command *command_list; // Liste des commandes générées par le parser
-	pid_t last_process_id;	 // PID du dernier processus exécuté
-	int wait_status;			 // Statut de la dernière commande exécutée
-	char *command_path;		 // Chemin complet vers l'exécutable de la commande
-	char **env_vars;			 // Tableau des variables d'environnement
-	char *heredoc_tempfile;	 // Fichier temporaire pour le heredoc
-	int saved_stdin_fd;		 // Descripteur de fichier pour sauvegarder stdin
-	int last_exit_status;	 // Statut de sortie de la dernière commande exécutée
-	t_env *env_var_list;		 // Liste des variables d'environnement
+	char *user_input;
+	t_token *token_list;
+	t_command *command_list;
+	pid_t last_process_id;
+	int wait_status;
+	char *command_path;
+	char **env_vars;
+	char *heredoc_tempfile;
+	int saved_stdin_fd;
+	int last_exit_status;
+	t_env *env_var_list;
 } t_shell;
 
 /*
 -------------------------------------------------------------
-|                       FONCTIONS                            |
+|                       FONCTIONS                           |
 -------------------------------------------------------------
 */
 
-// PARSING
-// Tokens
+/* Parsing */
 t_token *ft_create_token(int type, char *value);
 void ft_add_token(t_shell *shell, t_token *new_token);
-// Fonctions pour create et add simultanément un token
 void ft_create_add_token(t_shell *shell, int type, char *value);
 void ft_free_cmd_args(char **cmd_args);
 
-// Utils
+/* Utils */
 int ft_skip_whitespace(char *input, int i);
 int ft_handle_quotes(char *input, int i, char *quote_char);
-t_token_type ft_determine_token_type(char *input, int start, int is_first_token);
+t_token_type ft_determine_token_type(char *input, int start, int is_first_token); // TODO check if it's needed
 void ft_tokenize_input(char *input, t_shell *shell);
+int lexer(t_shell *shell);
+int parser(t_shell *shell);
 
-char **parse_input(char *input); // Fonction pour analyser l'entrée utilisateur
-int lexer(t_shell *shell);			// Generation des tokens
-int parser(t_shell *shell);		// Generation de l'AST
-
-// Exec
-void execute_command(t_command *cmd); // Fonction pour exécuter une commande
-int executor(t_shell *shell);
-// Built-In
-void ft_export(t_shell *shell);
-
+/* Execution */
 int init_shell(t_shell *shell, char **env);
 
-// Env Var
+/* Variables d'environnement */
 t_env *init_env_vars(char **env);
 t_env *init_default_env_vars(void);
 t_env *create_env_var_node(char *env_str);
 t_env *create_default_env_var_node(char *var_name, char *value);
-t_env *init_default_env_vars(void);
 t_env *ft_get_env_var_by_name(t_env *head, const char *name);
 char *ft_getenv(t_env *env_list, const char *name);
 void ft_setenv(t_env **env_list, const char *name, const char *value);
 void add_env_var_to_list(t_env **head, t_env *new_var);
 void global_exit_env(t_shell *shell, int status);
 void free_env_var_list(t_env *env);
-int lexer(t_shell *shell);
-int parser(t_shell *shell);
+
+/* Gestion des signaux */
 void handle_sigint(int sig);
 void setup_signal_handlers(void);
 void print_error(char *cmd, char *error_message);
-// void ft_get_all_env_vars(t_env *env_list);
 
-// Builtin functions
+/* Fonctions intégrées (Built-In) */
 int ft_builtin_cd(t_command *cmd, t_shell *shell);
 int ft_builtin_echo(t_command *cmd);
 int ft_builtin_env(t_env *env);
-int ft_builtin_exit();
+int ft_builtin_exit(void);
 int ft_builtin_export(t_command *cmd, t_env *env_list);
 int ft_builtin_pwd(t_command *commands);
 int ft_builtin_unset(t_command *cmd, t_env **env_list);
 
-// Executor functions
+/* Fonctions d'exécution */
 int ft_executor(t_shell *shell, char **env);
 char **ft_construct_cmd_args(char *cmd_name, char **cmd_args);
 char *ft_get_path(t_command *current, t_shell *shell);
@@ -168,9 +158,10 @@ void handle_redirections(t_command *current, int prev_fd);
 void ft_pipe(t_command *current, t_shell *shell);
 void ft_free_array(char **split);
 
+/* Fonctions utilitaires */
 void free_shell(t_shell *shell);
 int ft_heredoc_handler(t_command *command, t_shell *shell);
-void ft_close_all_fds(t_shell *shell);
 int ft_isspace(char c);
 void handle_exit(t_shell *shell);
+
 #endif // MINISHELL_H
