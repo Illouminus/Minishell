1f344a128553df89076f3fcb3e7ef7beed1f93dc
Adrien Hors <adrienhors1@gmail.com>
2024-08-13 10:23:06 +0200

    adding changes for merge

--- src/parser/expander/expander.c
+++ src/parser/expander/expander.c
@@ -6,13 +6,15 @@
 /*   By: adrienhors <adrienhors@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 09:03:53 by edouard           #+#    #+#             */
-/*   Updated: 2024/08/12 15:41:03 by adrienhors       ###   ########.fr       */
+/*   Updated: 2024/08/08 14:27:09 by adrienhors       ###   ########.fr       */
+
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *expander(char *str, t_shell *shell)
+
+char *get_env_value(const char *var_name, t_shell *shell)
 {
 	t_env *env_var;
 
@@ -44,8 +46,52 @@ int estimate_final_length(const char *str, t_shell *shell)
 		}
 		else
 		{
-			result[j++] = str[i++];
+			len++;
+			i++;
 		}
+	}
+	return len;
+}
+
+void handle_env_variable(const char *str, int *i, char **result, int *j, int *len, t_shell *shell)
+{
+	char *var_name;
+	char *env_value;
+	int env_len;
+
+	(*i)++;
+	var_name = extract_var_name(str, i);
+	env_value = get_env_value(var_name, shell);
+	free(var_name);
+	if (env_value)
+	{
+		env_len = ft_strlen(env_value);
+		ft_strncpy(&(*result)[*j], env_value, env_len);
+		*j += env_len;
+		*len += env_len;
+	}
+}
+
+char *ft_expander(char *str, t_shell *shell)
+{
+	int i;
+	int j;
+	int final_len;
+	char *result;
+
+	i = 0;
+	j = 0;
+	final_len = estimate_final_length(str, shell);
+	result = malloc(final_len + 1);
+	if (!result)
+		return NULL;
+
+	while (str[i])
+	{
+		if (str[i] == '$' && is_var_char(str[i + 1]))
+			handle_env_variable(str, &i, &result, &j, &final_len, shell);
+		else
+			add_char_to_result(&result, &j, str[i++]);
 	}
 	result[j] = '\0';
 	return result;
