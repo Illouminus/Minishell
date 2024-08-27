c21fa336cfc5e121f46c83e2032cdfc92b0e12b9
Adrien Hors <adrienhors1@gmail.com>
2024-08-27 14:39:21 +0200

    Parser Refacto - Done

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: adrienhors <adrienhors@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 12:18:35 by edouard           #+#    #+#             */
-/*   Updated: 2024/08/27 12:20:07 by adrienhors       ###   ########.fr       */
+/*   Updated: 2024/08/27 14:38:43 by adrienhors       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,12 +164,8 @@ void ft_parser_handle_command(t_token *current_token, char *cmd_value_clean, t_s
     t_command *new_command;
 
 	new_command = NULL; 
-    // Déterminer le nombre d'arguments pour la commande actuelle
     cmd_nb_args = ft_determine_nb_args(current_token);
-    
-    // Initialiser la nouvelle commande
     new_command = ft_new_command_init(new_command, cmd_nb_args, cmd_value_clean, shell);
-    // Si une commande précédente existe, on la relie à la nouvelle
     if (*last_command) 
 	{
         (*last_command)->next_cmd = new_command;
@@ -177,63 +173,77 @@ void ft_parser_handle_command(t_token *current_token, char *cmd_value_clean, t_s
     } 
 	else 
         shell->command_list = new_command;
-    // Mettre à jour le pointeur de la dernière commande
     *last_command = new_command;
 }
 
-// Déclaration de la nouvelle fonction
 int ft_parser_handle_empty_command(char *cmd_value_clean, t_token **current_token) 
 {
     if (*cmd_value_clean == '\0' && (*current_token)->next_tok) 
 	{
         *current_token = (*current_token)->next_tok;
         (*current_token)->tok_type = TOKEN_TYPE_CMD;
-        return 1;  // Indique qu'on doit continuer avec le prochain token
+        return (1); 
     }
-    return (0);  // Indique qu'on ne doit pas sauter de token
+    return (0);
 }
 
 
-int ft_check_user_input(t_shell *shell) {
+int ft_check_user_input(t_shell *shell) 
+{
     if (!shell->user_input || ft_strcmp(shell->user_input, "/0") == 0)
         return (EXIT_FAILURE);
     return (EXIT_SUCCESS);
 }
 
-// Identfy commands and their arguments. Recognitiion of operators and pipes. Build an AST at the end
-int parser(t_shell *shell)
+char *ft_handle_token_expansion(t_token *current_token, t_shell *shell, int *inside_single_quote) 
 {
-	t_token *current_token;
-	t_command *last_command;
-	char *cmd_value_clean;
-	int i;
-	int inside_single_quote; 
+    *inside_single_quote = 0;
+    return ft_expander(ft_clean_token_value(current_token->tok_value, inside_single_quote), shell, *inside_single_quote);
+}
 
-	last_command = NULL;
-	current_token = shell->token_list;
-	if (ft_check_user_input(shell) == EXIT_FAILURE)
-        return (EXIT_FAILURE);
-	while (current_token)
+
+void ft_parser_process_token(t_token **current_token, t_shell *shell, t_command **last_command, int *i) 
+{
+    int inside_single_quote;
+    char *cmd_value_clean; 
+
+	cmd_value_clean = ft_handle_token_expansion(*current_token, shell, &inside_single_quote);
+    if (ft_parser_handle_empty_command(cmd_value_clean, current_token))
+        return;
+
+    if ((*current_token)->tok_type == TOKEN_TYPE_CMD) 
 	{
-		inside_single_quote = 0;
-		cmd_value_clean = ft_expander(ft_clean_token_value(current_token->tok_value, &inside_single_quote), shell, inside_single_quote);
-		if (ft_parser_handle_empty_command(cmd_value_clean, &current_token))
-			continue;
-		if (current_token->tok_type == TOKEN_TYPE_CMD)
-		{
-			ft_parser_handle_command(current_token, cmd_value_clean, shell, &last_command);
-			i = 0;
-		} 
-		else if (current_token->tok_type == TOKEN_TYPE_REDIR_IN || current_token->tok_type == TOKEN_TYPE_REDIR_OUT || current_token->tok_type == TOKEN_TYPE_REDIR_APPEND || current_token->tok_type == TOKEN_TYPE_HEREDOC)
-			ft_parser_handle_redirection(&current_token, shell, last_command, &inside_single_quote); 
-		else if (current_token->tok_type == TOKEN_TYPE_ARG && (current_token->prev_tok->tok_type != TOKEN_TYPE_REDIR_IN && current_token->prev_tok->tok_type != TOKEN_TYPE_REDIR_OUT))
-		{
-			last_command->cmd_args[i] = ft_expander(cmd_value_clean, shell, inside_single_quote);
-			i++;
-		}
-		current_token = current_token->next_tok;
-	}
-	if (last_command && last_command->cmd_args)
-		last_command->cmd_args[i] = NULL;
-	return (EXIT_SUCCESS);
+        ft_parser_handle_command(*current_token, cmd_value_clean, shell, last_command);
+        *i = 0;
+    } 
+    else if ((*current_token)->tok_type == TOKEN_TYPE_REDIR_IN || (*current_token)->tok_type == TOKEN_TYPE_REDIR_OUT || (*current_token)->tok_type == TOKEN_TYPE_REDIR_APPEND || (*current_token)->tok_type == TOKEN_TYPE_HEREDOC) 
+        ft_parser_handle_redirection(current_token, shell, *last_command, &inside_single_quote);
+    else if ((*current_token)->tok_type == TOKEN_TYPE_ARG && ((*current_token)->prev_tok->tok_type != TOKEN_TYPE_REDIR_IN && (*current_token)->prev_tok->tok_type != TOKEN_TYPE_REDIR_OUT)) 
+	{
+        (*last_command)->cmd_args[*i] = ft_expander(cmd_value_clean, shell, inside_single_quote);
+        (*i)++;
+    }
+    *current_token = (*current_token)->next_tok;
+}
+
+
+// Identfy commands and their arguments. Recognitiion of operators and pipes. Build an AST at the end
+int parser(t_shell *shell) 
+{
+    t_token *current_token;
+    t_command *last_command;
+    int i;
+
+    last_command = NULL;
+    current_token = shell->token_list;
+
+    if (ft_check_user_input(shell) == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+
+    while (current_token)
+        ft_parser_process_token(&current_token, shell, &last_command, &i);
+
+    if (last_command && last_command->cmd_args)
+        last_command->cmd_args[i] = NULL;
+    return (EXIT_SUCCESS);
 }
