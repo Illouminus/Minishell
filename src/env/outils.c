a87659568f6e6ce6ac7ba372df98601e8b798c75
Edouard Baillot <ebaillot@made-f0Ar12s11.clusters.42paris.fr>
2024-09-04 10:57:36 +0200

    norminete env done

--- src/env/outils.c
+++ src/env/outils.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   outils.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edouard <edouard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ebaillot <ebaillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 11:51:04 by edouard           #+#    #+#             */
-/*   Updated: 2024/08/21 11:52:12 by edouard          ###   ########.fr       */
+/*   Updated: 2024/09/04 10:56:27 by ebaillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void free_env_node(t_env *env)
+static void	free_env_node(t_env *env)
 {
 	free(env->env_var_name);
 	free(env->env_value);
 	free(env);
 }
 
-void free_env_var_list(t_env *env)
+void	free_env_var_list(t_env *env)
 {
-	t_env *current;
-	t_env *next;
+	t_env	*current;
+	t_env	*next;
 
 	current = env;
 	while (current)
@@ -33,13 +33,13 @@ void free_env_var_list(t_env *env)
 	}
 }
 
-t_env *ft_get_env_var_by_name(t_env *head, const char *name)
+t_env	*ft_get_env_var_by_name(t_env *head, const char *name)
 {
 	while (head != NULL)
 	{
 		if (ft_strcmp(head->env_var_name, name) == 0)
-			return head;
+			return (head);
 		head = head->next_env;
 	}
-	return NULL;
+	return (NULL);
 }
